09020d5c3ec010d12390d97cfbff3ded086ef8f4
Alok RG <alokratan277@gmail.com>
2022-07-07 17:55:40 +0530

    hell

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import  style from '../styles/Home.module.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import {Link} from 'next/link'
 
 
 export default function Home() {
@@ -19,25 +20,25 @@ export default function Home() {
 <div className={style.menu}>
   <ul>
     <li>
-      <a href='/'><i className="bi bi-camera-reels-fill"></i>LIVE</a></li>
-    <li><a href='/'><i className="bi bi-postcard-fill"></i>MEMBERSHIP</a></li>
-    <li><a href='/'><i className="bi bi-clipboard-data-fill"></i>PLANS</a></li>
-    <li><a href='/'><i className="bi bi-grid-fill"></i>MORE</a></li>
+      <Link href='/'><i className="bi bi-camera-reels-fill"></i>LIVE</Link></li>
+    <li><Link href='/'><i className="bi bi-postcard-fill"></i>MEMBERSHIP</Link></li>
+    <li><Link href='/'><i className="bi bi-clipboard-data-fill"></i>PLANS</Link></li>
+    <li><Link href='/'><i className="bi bi-grid-fill"></i>MORE</Link></li>
   </ul>
 </div>
-<div className={style.login}><p><i class="bi bi-person-fill"></i>Login</p></div>
+<div className={style.login}><p><i className="bi bi-person-fill"></i>Login</p></div>
 </div>
 <div className={style.workout}>
 </div>
 
 <div className={style.containers}>
 <h1>
-  Runner's Workout
+  Runners Workout
 </h1>
 <div className={style.slider}>
-<div className={style.strength}><h5>Strength</h5><i class="bi bi-arrow-right-circle-fill"></i></div>
-<div className={style.mobility}><h5>Mobility</h5><i class="bi bi-arrow-right-circle-fill"></i></div>
-<div className={style.drills}><h5>Drills</h5><i class="bi bi-arrow-right-circle-fill"></i></div>
+<div className={style.strength}><h5>Strength</h5><i className="bi bi-arrow-right-circle-fill"></i></div>
+<div className={style.mobility}><h5>Mobility</h5><i className="bi bi-arrow-right-circle-fill"></i></div>
+<div className={style.drills}><h5>Drills</h5><i className="bi bi-arrow-right-circle-fill"></i></div>
 </div>
 </div>
 
